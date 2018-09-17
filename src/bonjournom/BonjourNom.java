9f5c66d0fda5a39f3cc50e6ce011878826fe6248
hd691026 <hd691026@C39486.ens.uqam.ca>
2018-09-17 10:09:40 -0400

    Initial commit

---
+++ src/bonjournom/BonjourNom.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonjournom;
+
+/**
+ *
+ * @author hd691026
+ */
+public class BonjourNom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour John Doe!");
+        if(args.length == 2){
+            String prenom = args[0];
+            String nom =args[1];
+            System.out.println("Bonjour " + prenom + " " + nom);
+        }else{
+            System.out.println("Nombre d'arguments invalide");
+        }
+    }
+    
+}
