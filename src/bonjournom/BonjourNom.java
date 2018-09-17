5ecd92774552a26726f5dc875b3aec1317f55a88
hd691026 <hd691026@C39486.ens.uqam.ca>
2018-09-17 11:02:05 -0400

    Commit 4

--- src/bonjournom/BonjourNom.java
+++ src/bonjournom/BonjourNom.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package bonjournom;
-
+import aleatoire.Person;
 /**
  *
  * @author hd691026
@@ -17,13 +17,28 @@ public class BonjourNom {
     public static void main(String[] args) {
         System.out.println("Hello World2!");
         System.out.println("Bonjour John Doe!");
-        if(args.length == 2){
-            String prenom = args[0];
-            String nom =args[1];
+        //if(args.length == 2){
+           String prenom = args[0];
+           String nom =args[1];
+           String ageString = args[2];
             System.out.println("Bonjour " + prenom + " " + nom);
-        }else{
-            System.out.println("Nombre d'arguments invalide");
-        }
+        //}else{
+          //  System.out.println("Nombre d'arguments invalide");
+        //}
+        
+        Person personne = new Person("Bill","Gates", 60);
+        System.out.println("First Name: " + personne.getFirstName());
+        System.out.println("Last Name: " + personne.getLastName());
+        System.out.println("Age:  " + personne.getAge());
+        
+        personne.setFirstName(prenom);
+        personne.setLastName(nom);
+        int age = Integer.parseInt(ageString);
+        personne.setAge(age);
+     
+        System.out.println("First Name: " + personne.getFirstName());
+        System.out.println("Last Name: " + personne.getLastName());
+        System.out.println("Age:  " + personne.getAge());
     }
     
 }
